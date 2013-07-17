83f9d7dbe72d761d08548f3a68cad3d3847ed0bd
John Doe <l.c.schoening@me.com>
2013-07-17 13:39:40 +0100

    use on_activated instead on_load

--- Preference Helper.py
+++ Preference Helper.py
@@ -7,20 +7,21 @@ if int(sublime.version()) < 3000:
 else:
 	from .Tools import *
 
+
 class PreferenceHelperListener(sublime_plugin.EventListener):
 
-	def on_load(self, view):
-		if not view or not IsSublimeSetting(view):
-			return []
-		settings = sublime.load_settings("Preference Helper.sublime-settings")
-		package_name = PackageName(view)
-		if package_name != "Default" and settings.get("protect_default_settings", True) and not IsUserSublimeSetting(view):
-			exclude_packages = settings.get("exclude_packages", [])
-			view.set_read_only(package_name not in exclude_packages)
+	def on_activated(self, view):
+		
+		if IsSublimeSetting(view):
+			settings = sublime.load_settings("Preference Helper.sublime-settings")
+			package_name = PackageName(view)
+			if package_name != "Default" and settings.get("protect_default_settings", True) and not IsUserSublimeSetting(view):
+				exclude_packages = settings.get("exclude_packages", [])
+				view.set_read_only(package_name not in exclude_packages)
 
 	def on_query_completions(self, view, prefix, locations):
 			
-		if not view or not IsSublimeSetting(view) or not view.score_selector(locations[0], "source.json") or view.score_selector(locations[0], "source.json string.quoted.double.json") != 2064:
+		if not IsSublimeSetting(view) or view.score_selector(locations[0], "source.json string.quoted.double.json") != 2064:
 			return []
 		src_json = DefaultSublimeSetting(view)
 		# print(src_json)
@@ -30,9 +31,11 @@ class PreferenceHelperListener(sublime_plugin.EventListener):
 		# print(keys)
 		return keys, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS
 
+
 class FillSettingFileCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit):
+		
 		point = self.view.sel()[0].a if self.view.sel()[0].a < self.view.sel()[0].b else self.view.sel()[0].b
 		line = self.view.substr(sublime.Region(self.view.line(point).a, point))[::-1]
 
@@ -50,9 +53,11 @@ class FillSettingFileCommand(sublime_plugin.TextCommand):
 				self.view.sel().add(point + len(s))
 				self.view.show(point + len(s))
 
+
 class OpenSettingFileCommand(sublime_plugin.WindowCommand):
 
 	def run(self):
+		
 		resources = [resource[9:] for resource in FindResources("*.sublime-settings")]
 		def on_done(i):
 			if i >= 0:
@@ -60,16 +65,18 @@ class OpenSettingFileCommand(sublime_plugin.WindowCommand):
 
 		self.window.show_quick_panel(resources, on_done)
 
+
 class ToggleSettingFileCommand(sublime_plugin.TextCommand):
 
 	def is_enabled(self):
 		return IsSublimeSetting(self.view)
 		
 	def run(self, edit):
+		
 		file_dir, file_name = os.path.split(self.view.file_name())
-		package_name = PackageName(self.view)
 		
 		if IsUserSublimeSetting(self.view):
+			package_name = PackageName(self.view)
 			self.view.window().run_command("open_file", { "file": "${packages}/%s/%s" % (package_name, file_name) })
 		else:
 			self.view.window().run_command("open_file", { "file": "${packages}/User/%s" % file_name })
