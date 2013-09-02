6333bcfe8b3004bb2a837ce06c7f7921322187b0
Chris--- <l.c.schoening@gmail.com>
2013-09-02 06:49:30 +0100

    add sublime-command for disable read-only flag for selected package

--- Preference Helper.py
+++ Preference Helper.py
@@ -11,16 +11,16 @@ else:
 class PreferenceHelperListener(sublime_plugin.EventListener):
 
 	def on_activated(self, view):
-		
+
 		if IsSublimeSetting(view):
 			settings = sublime.load_settings("Preference Helper.sublime-settings")
 			package_name = PackageName(view)
-			if package_name != "Default" and settings.get("protect_default_settings", True) and not IsUserSublimeSetting(view):
+			if package_name != "Default" and settings.get("protect_default_settings", True) and not IsUserSublimeSetting(view) and not view.settings().get("pref_exclude_package"):
 				exclude_packages = settings.get("exclude_packages", [])
 				view.set_read_only(package_name not in exclude_packages)
 
 	def on_query_completions(self, view, prefix, locations):
-			
+
 		if not IsSublimeSetting(view) or view.score_selector(locations[0], "source.json string.quoted.double.json") != 2064:
 			return []
 		src_json = DefaultSublimeSetting(view)
@@ -35,7 +35,7 @@ class PreferenceHelperListener(sublime_plugin.EventListener):
 class PrefFillSettingFileCommand(sublime_plugin.TextCommand):
 
 	def run(self, edit):
-		
+
 		point = self.view.sel()[0].a if self.view.sel()[0].a < self.view.sel()[0].b else self.view.sel()[0].b
 		line = self.view.substr(sublime.Region(self.view.line(point).a, point))[::-1]
 
@@ -47,7 +47,7 @@ class PrefFillSettingFileCommand(sublime_plugin.TextCommand):
 				s = json.dumps({key:src_json[key]}, indent=2).strip("{}").strip("\t\n ")
 				s = s.replace("\"%s\"" % key, "")
 				s = "%s" % re.sub(r"\n\s{%d}" % 2, "\n%s" % Indention(line[::-1]), s)
-				# Insert default value 
+				# Insert default value
 				self.view.insert(edit, point, s)
 				self.view.sel().clear()
 				self.view.sel().add(point + len(s))
@@ -57,7 +57,7 @@ class PrefFillSettingFileCommand(sublime_plugin.TextCommand):
 class PrefOpenSettingFileCommand(sublime_plugin.WindowCommand):
 
 	def run(self):
-		
+
 		resources = [resource[9:] for resource in FindResources("*.sublime-settings")]
 		def on_done(i):
 			if i >= 0:
@@ -70,13 +70,39 @@ class PrefToggleSettingFileCommand(sublime_plugin.TextCommand):
 
 	def is_enabled(self):
 		return IsSublimeSetting(self.view)
-		
+
 	def run(self, edit):
-		
+
 		file_dir, file_name = os.path.split(self.view.file_name())
-		
+
 		if IsUserSublimeSetting(self.view):
 			package_name = PackageName(self.view)
 			self.view.window().run_command("open_file", { "file": "${packages}/%s/%s" % (package_name, file_name) })
 		else:
 			self.view.window().run_command("open_file", { "file": "${packages}/User/%s" % file_name })
+
+
+class PrefExcludePackageCommand(sublime_plugin.TextCommand):
+
+	def is_enabled(self):
+		return IsSublimeSetting(self.view) and not IsUserSublimeSetting(self.view)
+
+	def run(self, edit, save_to_settings = True):
+
+		# Get package name
+		package_name = PackageName(self.view)
+
+		if save_to_settings:
+			# Load settings if save_to_settings
+			s = sublime.load_settings("Preference Helper.sublime-settings")
+			exclude_packages = s.get("exclude_packages", [])
+			if package_name not in exclude_packages:
+				exclude_packages += [package_name]
+			s.set("exclude_packages", exclude_packages)
+			sublime.save_settings("Preference Helper.sublime-settings")
+		else:
+			self.view.settings().set("pref_exclude_package", True)
+
+		# set_read_only
+		self.view.set_read_only(False)
+
